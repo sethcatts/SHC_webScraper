d56fdfc1ca5b7706fc590dd0f45ca17810ed3ff4
thefarwolf <seth.h.catts@gmail.com>
2018-07-15 01:58:35 -0400

    Added function that saves scrapes in a report folder

--- scraper.py
+++ scraper.py
@@ -2,32 +2,39 @@
     # TODO: Fix the way the array is returned in getKeywords function
     # TODO: Add error checking for unfound file or an empty file/array
 # TODO: Save distinct reports
-    #Date and time
+    # TODO: Date and Time - Complete
 # TODO: Run daily
     #...?
-# TODO: Text on new news report about keywords supplied
-    #Module?
+# TODO: Text alert to phone on new news reports about keywords supplied
+    #texting module?
 import os
 import csv
+import time
 import urllib.request
 from bs4 import BeautifulSoup
 
-#Take a file path and return the contents of the file as a keyword array
+#Save report to specified directory
 def saveReport(returnedContentArray, folderPath):
-    pass
+    reportNumber = time.asctime(time.localtime(time.time())).replace(":", "")
+    with open(folderPath + reportNumber + '.txt', "w") as f:
+        f.write("\n".join(returnedContentArray))
+
+#Take a file path and return the contents of the file as a keyword array
 def getFileContents(filePath):
     contentArray = []
     with open(filePath, "r") as f:
         r = csv.reader(f, delimiter = ",")
         for x in r:
             contentArray.append(x)
-    print("Created content array:", contentArray[0])
     return contentArray[0]
+
 class Scraper:
-    def __init__(self, sitesFile, keywordsFile):
+    def __init__(self, sitesFile, keywordsFile, reportPath):
         self.sites = getFileContents(sitesFile)
         self.keys = getFileContents(keywordsFile)
+        self.reportPath = reportPath
         self.returnedContent = []
+
     def scrapeSites(self):
         for x in range(len(self.sites)):
             print(self.sites[x])
@@ -37,6 +44,7 @@ class Scraper:
             for tag in bfsp.find_all('a'):
                 url = tag.get('href')
                 if url and 'html' and self.keys[4] in url:
-                    self.returnedContent.append(url)
+                    self.returnedContent.append(self.sites[x] + url)
         print(self.returnedContent)
-Scraper("websites.csv", "keywords.csv").scrapeSites()
+        saveReport(self.returnedContent, self.reportPath)
+Scraper("websites.csv", "keywords.csv", "reports/").scrapeSites()
