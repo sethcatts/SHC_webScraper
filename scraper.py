49c59c371877e20b96abc1ab07d0e31897271f20
thefarwolf <seth.h.catts@gmail.com>
2018-07-17 22:17:04 -0400

    Adding rubberducky comments to scrape method

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 # TODO: Allow for keywords in initalization of scraper
     # TODO: Add error checking for unfound file or an empty file/array
 # TODO: Save distinct reports
-    # TODO: Date and Time - Complete
+    # TODO: Date and Time added as header to reports
     # TODO: Add text divide for each site for readability
     # TODO: Add tags/details about why the article was added
     # TODO: Collect headline for report as well as link
+    # TODO: What if the folder path is null?
         # TODO: This may have to be site specific depending on tags used
 # TODO: Run daily
     #...?
@@ -15,10 +16,17 @@
     # TODO: Create site list
     # TODO: Test lists for speed and accuracy
 # TODO: Refactoring
-    # TODO: getFileContents is just f***ing terrible -> refactor
+    # TODO: getFileContents is just f***ing terrible
     # TODO: Convert functions to class methods
+    # TODO: When reading CSV files the reader only reads the
+           #first term of each line
+    # TODO: Remove rubberducky comments
 
-#Note to self: It is the year of our lord 2018, it's okay to add some forloops
+#Note to self: It is the year of our lord 2018, it's
+#okay to add some forloops
+
+#Just not this many...
+
 import csv
 import time
 import urllib.request
@@ -30,7 +38,8 @@ def saveReport(returnedContentArray, folderPath):
     with open(folderPath + reportNumber + '.txt', "w") as f:
         f.write("\n".join(returnedContentArray))
 
-#Take a file path and return the contents of the file as a content array
+#Take a CSV file and return the contents of the file as a content array
+#(File must be one piece of content per line)
 def getFileContents(filePath):
     fileContents = []
     contentArray = []
@@ -50,14 +59,33 @@ class Scraper:
         self.returnedContent = []
     def scrapeSites(self):
         for x in range(len(self.sites)):
+            #DBG
             print("Accessing site: ", self.sites[x])
+
+            #Make responce equal to site data
             response = urllib.request.urlopen(self.sites[x])
+
+            #Make the file into one text string for parsing
             html = response.read()
+
+            #parse the string using bfsp html parser
             bfsp = BeautifulSoup(html, "html.parser")
+
+            #For all the link tagged parts of the object do x
+            #find_all is returning an array of link tags to loop over
             for tag in bfsp.find_all('a'):
+
+                #Make a variable equal to the value of the href subtag
                 url = tag.get('href')
+                #for all keys in the keyterm list
                 for y in range(len(self.keys)):
+
+                    #if the url is an actuall HTML doc and
+                    #contains a key it is added
                     if url and 'html' and self.keys[y] in url:
+
+                        #Append the full url to the returnedContentArray
+                        #to be saved after all the requests have been made
                         self.returnedContent.append(self.sites[x] + url)
         print(self.returnedContent)
         saveReport(self.returnedContent, self.reportPath)
